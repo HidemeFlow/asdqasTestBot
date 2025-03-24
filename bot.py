07af610512c29d0d2e983a328093db27228a6d59
HidemeFlow <145919878+HidemeFlow@users.noreply.github.com>
2025-03-24 20:27:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
+import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram.types import Message
+from aiogram.filters import Command
 
 TOKEN = "7556829484:AAESxC6eNoq5sdAF8MCIyUN3iC2S3QDlheE"
 
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.reply("Привіт! Я вічний безкоштовний бот.")
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer("Привіт! Я оновлений бот на aiogram 3!")
+
+async def main():
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
